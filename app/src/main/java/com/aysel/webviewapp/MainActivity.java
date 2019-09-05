0aafdb730b909a8a0159d286a9086d91dab0bd56
Aycell <ayselaydin01@gmail.com>
2019-09-05 22:34:52 +0300

    ..

--- app/src/main/java/com/aysel/webviewapp/MainActivity.java
+++ app/src/main/java/com/aysel/webviewapp/MainActivity.java
@@ -1,17 +1,12 @@
 package com.aysel.webviewapp;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -30,59 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
         mProgressBar.setVisibility(View.VISIBLE);
         mwebview.getSettings().setJavaScriptEnabled(true);
-        mwebview.getSettings().setAllowFileAccess(true);
-        mwebview.setWebViewClient(new Browser_home());
         mwebview.getSettings().setDomStorageEnabled(true);
         mwebview.loadUrl("https://gelecegiyazanlar.turkcell.com.tr");
-        mwebview.setWebChromeClient(new MyChrome());
-
-        mwebview.getSettings().setTextZoom(100); // 100% is maximum here
-        mwebview.getSettings().setAppCacheEnabled(true);
-        mwebview.getSettings().setBuiltInZoomControls(true);
-        mwebview.getSettings().setSaveFormData(true);
-
-        mwebview.setWebViewClient(new WebViewClient() {
-
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                mwebview.loadUrl("file:///android_asset/error.html");
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                mProgressBar.setVisibility(View.GONE);
-                super.onPageFinished(view, url);
-            }
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if (url.contains("whatsapp")) {
-                    try {
-                        view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-                        return true;
-                    } catch (android.content.ActivityNotFoundException e) {
-                        Toast.makeText(getApplicationContext(), "Uygulama yüklü değil", Toast.LENGTH_LONG).show();
-                    }
-                }
-                return super.shouldOverrideUrlLoading(view, url);
-            }
-        });
+        mwebview.setWebViewClient(new Browser_home());
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (event.getAction() == KeyEvent.ACTION_DOWN) {
-            switch (keyCode) {
-                case KeyEvent.KEYCODE_BACK:
-                    if (mwebview.canGoBack()) {
-                        mwebview.goBack();
-                    }
-                    return true;
-            }
-        } else {
-            finish();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
+    public void onBackPressed() {
+        if (mwebview != null && mwebview.canGoBack())
+            mwebview.goBack();
+        else
+            super.onBackPressed();
     }
 
     private class Browser_home extends WebViewClient {
@@ -90,9 +43,8 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            super.onPageStarted(view, url, favicon);
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            mwebview.loadUrl("file:///android_asset/error.html");
         }
 
         @Override
@@ -100,47 +52,18 @@ public class MainActivity extends AppCompatActivity {
             mProgressBar.setVisibility(View.GONE);
             super.onPageFinished(view, url);
         }
-    }
 
-    private class MyChrome extends WebChromeClient {
-
-        private View mCustomView;
-        private WebChromeClient.CustomViewCallback mCustomViewCallback;
-        protected FrameLayout mFullscreenContainer;
-        private int mOriginalOrientation;
-        private int mOriginalSystemUiVisibility;
-
-        MyChrome() {
-        }
-
-        public Bitmap getDefaultVideoPoster() {
-            if (mCustomView == null) {
-                return null;
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.contains("whatsapp")) {
+                try {
+                    view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                    return true;
+                } catch (android.content.ActivityNotFoundException e) {
+                    Toast.makeText(getApplicationContext(), "Uygulama yüklü değil", Toast.LENGTH_LONG).show();
+                }
             }
-            return BitmapFactory.decodeResource(getApplicationContext().getResources(), 2130837573);
-        }
-
-        public void onHideCustomView() {
-            ((FrameLayout) getWindow().getDecorView()).removeView(this.mCustomView);
-            this.mCustomView = null;
-            getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
-            setRequestedOrientation(this.mOriginalOrientation);
-            this.mCustomViewCallback.onCustomViewHidden();
-            this.mCustomViewCallback = null;
-        }
-
-        public void onShowCustomView(View paramView, WebChromeClient.CustomViewCallback paramCustomViewCallback) {
-            if (this.mCustomView != null) {
-                onHideCustomView();
-                return;
-            }
-            this.mCustomView = paramView;
-            this.mOriginalSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
-            this.mOriginalOrientation = getRequestedOrientation();
-            this.mCustomViewCallback = paramCustomViewCallback;
-            ((FrameLayout) getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
-            getWindow().getDecorView().setSystemUiVisibility(3846);
+            return super.shouldOverrideUrlLoading(view, url);
         }
     }
-
 }
